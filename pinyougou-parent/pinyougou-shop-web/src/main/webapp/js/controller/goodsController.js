8ce0afc9bafbeccc55d3820899aa2682bd916a55
RookieWangZhiWei <RookieWangZhiWei@163.com>
2018-11-15 23:38:04 +0800

    商品管理1

--- pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService){	
+app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService,itemCatService,typeTemplateService){	
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -115,12 +115,60 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
     //添加图片列表
     $scope.add_image_entity=function(){    	
         $scope.entity.goodsDesc.itemImages.push($scope.image_entity);
+       
     }
     
     
     $scope.remove_image_entity=function(index){
 	    $scope.entity.goodsDesc.itemImages.splice(index,1);
 }
-
+    	
+    $scope.selectItemCat1List=function(){
+        itemCatService.findByParentId(0).success(
+      		 function(response){
+      			 $scope.itemCat1List=response; 
+      		 }
+        );
+  }
+    
+    $scope.$watch('entity.goods.category1Id',function(newValue,oldValue){
+    	itemCatService.findByParentId(newValue).success(
+    			function(response){
+    				$scope.itemCat2List=response;
+    			}
+    	);
+    });
+    
+    
+    $scope.$watch('entity.goods.category2Id', function(newValue, oldValue) {          
+    	//根据选择的值，查询二级分类
+    	itemCatService.findByParentId(newValue).success(
+    		function(response){
+    			$scope.itemCat3List=response; 	    			
+    		}
+    	);    	
+ });
+    
+   $scope.$watch('entity.goods.category3Id',function(newValue,oldValue){
+	  
+	   itemCatService.findOne(newValue).success(
+			  function(response){
+				  $scope.entity.goods.typeTemplateId=response.typeId;
+			  }
+	   );
+   });
+   
+   
+   
+   $scope.$watch('entity.goods.typeTemplateId',function(newValue,oldValue){
+	  
+	   typeTemplateService.findOne(newValue).success(
+			   function(response){
+				   $scope.typeTemplate=response;
+				   $scope.typeTemplate.brandIds=JSON.parse($scope.typeTemplate.brandIds);
+				   $scope.entity.goodsDesc.customAttributeItems=JSON.parse($scope.typeTemplate.customAttributeItems);
+			   }
+	   );
+   });
 
 });	
